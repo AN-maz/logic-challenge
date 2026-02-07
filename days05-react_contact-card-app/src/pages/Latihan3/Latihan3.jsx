6534c818dcd8028fcd8d0e8d9b8966f5c653b327
Andrian Maulana <andrianian398@gmail.com>
2026-02-07 22:31:49 +0700

    feat:addFolder-latihan3

--- days05-react_contact-card-app/src/pages/Latihan3/Latihan3.jsx
+++ days05-react_contact-card-app/src/pages/Latihan3/Latihan3.jsx
@@ -1,7 +1,51 @@
-function Latihan3(){
-    return(
-        <div className="containar">
-            <h1>MasPur Latihan 3</h1>
+import { useState } from 'react'
+import './Latihan3.css'
+
+function Latihan3() {
+
+    const [todos, setTodos] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+
+    const addTodo = () => {
+
+        if (inputValue.trim() !== '') {
+            setTodos([...todos, inputValue])
+            setInputValue('')
+        }
+
+    };
+
+    const deleteTodo = (index) => {
+        const newTodos = todos.filter((_, i) => i !== index);
+        setTodos(newTodos)
+    };
+
+    return (
+        <div className='todo-container'>
+            <h1>Todo List</h1>
+
+            <div className="input-group">
+                <input
+                    type="text"
+                    placeholder='Masukan sesuatu ke list...'
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    onKeyPress={(e) => e.key === 'Enter' && addTodo()} />
+                <button onClick={addTodo}>Tambah</button>
+            </div>
+
+            <ul className="todo-list">
+                {todos.map((item, index) => (
+                    <li key={index} className='todo-item'>
+                        <span>{item}</span>
+                        <button onClick={() => deleteTodo(index)} className='delete-btn'>
+                            &times;
+                        </button>
+                    </li>
+                ))}
+            </ul>
+
+            {todos.length === 0 && <p className='empty-state'>Belum ada list masuk...</p>}
         </div>
     );
 }
