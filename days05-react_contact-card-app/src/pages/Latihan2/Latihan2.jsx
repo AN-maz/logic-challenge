a72eb45528007e6e84894c9509c56f6f261f18e7
Andrian Maulana <andrianian398@gmail.com>
2026-02-07 21:55:01 +0700

    feat:addFolder-latihan2

--- days05-react_contact-card-app/src/pages/Latihan2/Latihan2.jsx
+++ days05-react_contact-card-app/src/pages/Latihan2/Latihan2.jsx
@@ -1,7 +1,41 @@
-function Latihan2(){
-    return(
+import './Latihan2.css'
+function ProductCard({ name, price, image }) {
+
+    const formatHarga = (angka) => {
+        return new Intl.NumberFormat('id-ID', {
+            style: 'currency',
+            currency: 'IDR',
+            minimumFractionDigits: 0
+        }).format(angka)
+    }
+    return (
+        <div className="product-cards">
+            <img src={image} alt={name} />
+            <h3>{name}</h3>
+            <h2>{formatHarga(price)}</h2>
+        </div>
+    );
+}
+function Latihan2() {
+    return (
         <div className="container">
-            <h1>MasPur Latihan2</h1>
+            <h1>Market Place</h1>
+
+            <div className="product-grid">
+                <ProductCard
+                    name="Laptop ASUS"
+                    price={1_200_000}
+                    image="https://picsum.photos/300/200?random=1"
+                />
+
+                <ProductCard
+                    name="Mouse Lim"
+                    price={450_000}
+                    image="https://picsum.photos/300/200?random=2"
+                />
+
+                
+            </div>
         </div>
     );
 }
